945ab88e7c5826e819a8b5f20b5a536fe0d76cc3
Sainfuw <sainfuw@aol.com>
2018-06-06 04:23:32 -0400

    ADD: ex5

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,11 @@
-# Crear un método que reciba como parámetro dos números enteros positivos
-# e imprima los números pares que existen entre esos dos números.
+# Crear un metodo que reciba como parametro dos numeros enteros positivos
+# e imprima los numeros pares que existen entre esos dos numeros.
+
+def pares(num1, num2)
+  while num1 <= num2
+    puts num1 if num1.even?
+    num1 += 1
+  end
+end
+
+pares(2, 10)
