1f7c388b7fc63b1ff7ee7930afd1defc96bbb1cb
Sainfuw <sainfuw@aol.com>
2018-06-06 04:38:35 -0400

    ADD: ex1 arrays

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -5,4 +5,18 @@
 # 4. Mostrar en pantalla todos los elementos junto con un índice.
 # 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
 
-arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+arreglo = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+puts arreglo[0]
+puts arreglo[10]
+arreglo.each do |dato|
+  print "#{dato}  "
+end
+puts
+arreglo.each_with_index do |dato, indice|
+  print "#{indice} - #{dato}  "
+end
+puts
+arreglo.each_with_index do |dato, indice|
+  print "#{dato}  " if indice.even?
+end
