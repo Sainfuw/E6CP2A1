c2b827c512adb809592c6ffe89b391c02728d8da
Sainfuw <sainfuw@aol.com>
2018-06-06 05:30:05 -0400

    ADD: ex2 arrays

--- 2 arrays/ejercicio2.rb
+++ 2 arrays/ejercicio2.rb
@@ -11,4 +11,18 @@
 
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
 
-a.class
+a.delete_at(a.first - 1)
+print a
+a.delete_at(a.length - 1)
+print a
+pos = if a.length.even?
+        a.length / 2 - 1
+      else
+        a.length / 2
+      end
+a.delete_at(pos)
+print a
+a.delete_at(a.length - 1) while a[a.length - 1] != 1
+print a
+b.push(a.pop) until a.empty?
+print a, b
